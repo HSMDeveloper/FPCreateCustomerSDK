f2185ed8771cdd4732737f6dfde9aba00cb6c645
HSM <hsadeghmoghaddasi@gmail.com>
2025-05-26 17:20:23 +0330

    binary target added

--- Package.swift
+++ Package.swift
@@ -9,13 +9,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FPCreateCustomerSDK",
-            targets: ["FPCreateCustomerSDK"]),
+            targets: ["CreateCustomerSDK"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "FPCreateCustomerSDK"),
+        .binaryTarget(name: "CreateCustomerSDK", path: "./Sources/FPCC.xcframework")
 
     ]
 )
